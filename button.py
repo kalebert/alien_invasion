4201f464cdb8d1ce7fc31887a0c9f220bc51768a
kalebert <evanschulte9@gmail.com>
2020-09-23 11:12:38 -0600

    Added Button to Screen

--- button.py
+++ button.py
@@ -14,9 +14,20 @@ class Button:
 		self.font = pygame.font.SysFont(None, 48)
 
 		# Build the button's rect object and center it. 
-		self.rect = pygame.rect(0, 0, self.width, self.height)
-		self.rect.center. = self.screen_rect.center
+		self.rect = pygame.Rect(0, 0, self.width, self.height)
+		self.rect.center = self.screen_rect.center
 
 		# The button message needs to be prepped only once
 		self._prep_msg(msg)
-		
+
+	def _prep_msg(self, msg):
+		"""Turn msg into rendered image and center text on the button."""
+		self.msg_image = self.font.render(msg, True, self.text_color, 
+				self.button_color)
+		self.msg_image_rect = self.msg_image.get_rect()
+		self.msg_image_rect.center = self.rect.center
+
+	def draw_button(self):
+		# Draw blank button and then draw message
+		self.screen.fill(self.button_color, self.rect)
+		self.screen.blit(self.msg_image, self.msg_image_rect)
--- game_stats.py
+++ game_stats.py
@@ -7,7 +7,7 @@ class GameStats:
 		self.reset_stats()
 
 		#Start Alien Invasion in an actice stage
-		self.game_active = True
+		self.game_active = False
 
 	def reset_stats(self):
 		"""Initialize statistics that can change during game."""
